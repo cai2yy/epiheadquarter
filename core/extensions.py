95cee12aae2856db53695f2546bf5b739eadabd7
jiaohuazhiyichong <112987845@qq.com>
2020-02-13 22:29:37 +0800

    更新model

---
+++ core/extensions.py
@@ -0,0 +1,27 @@
+from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_migrate import Migrate
+
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+csrf = CSRFProtect()
+ckeditor = CKEditor()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    from core.model import User
+    user = User.query.get(int(user_id))
+    return user
+
+
+login_manager.login_view = 'auth.login'
+# login_manager.login_message = 'Your custom message'
+login_manager.login_message_category = 'warning'
