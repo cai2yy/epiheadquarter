2281cbd82d54f11ad958050510c406feb81a6777
cai2yy <cai2yy@126.com>
2020-02-19 19:18:04 +0800

    修复了从命令行启动的问题

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
+os.chdir(os.getcwd())
 from dotenv import load_dotenv
 from backend import create_app  # noqa
 
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+print(os.getcwd())
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
