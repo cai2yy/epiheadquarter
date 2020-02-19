fa3c2277c320cc7c2765bbbf8a723ec1c08fffdd
YeChang <337894180@qq.com>
2020-02-19 21:30:10 +0800

    --项目结构

--- myapp/server/models/Article.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const schema = new mongoose.Schema({
-  title: { type: String },
-  content: { type: String }
-});
-
-module.exports = mongoose.model("Article", schema);
